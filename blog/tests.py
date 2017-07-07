75f964d6cb477665d553ffb2d10dccbbc0f21f23
Jack Bittiner <jackbittiner@hotmail.com>
2017-07-07 11:48:15 +0100

    Entry class returns title as string and tested

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Entry
+
+
+class EntryModelTest(TestCase):
+
+    def test_string_representation(self):
+        entry = Entry(title="My entry title")
+        self.assertEqual(str(entry), entry.title)
